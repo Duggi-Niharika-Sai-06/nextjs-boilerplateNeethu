565be8307d2a7d4ad5e11a8b7c029ea1bb92bb29
Duggi-Niharika-Sai-06 <60509922+Duggi-Niharika-Sai-06@users.noreply.github.com>
2022-11-08 09:55:59 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          This Page is owned By NDUGGI{' '}
+          This Page is owned By NDUGGI
           <code className={styles.code}>pages/index.js</code>
         </p>
 
